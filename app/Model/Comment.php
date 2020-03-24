12fd564d7703619e1e412709ceb8fd829cb5c9e1
HosseinAlikhani <poshtiban.esteam@gmail.com>
2020-03-25 04:24:43 +0430

    add: add relation of user and reply in comment.php and CommentReply.php model

--- app/Model/Comment.php
+++ app/Model/Comment.php
@@ -5,5 +5,17 @@ namespace App\Model;
 
 class Comment extends BaseModel
 {
-    //
+
+    public function user()
+    {
+        return $this->hasOne(User::class,
+            'id',
+            'user_id');
+    }
+    public function commentReply()
+    {
+        return $this->hasMany(CommentReply::class,
+            'comment_id',
+            'id');
+    }
 }
--- app/Model/CommentReply.php
+++ app/Model/CommentReply.php
@@ -5,5 +5,10 @@ namespace App\Model;
 
 class CommentReply extends BaseModel
 {
-    //
+    public function user()
+    {
+        return $this->hasOne(User::class,
+            'id',
+            'user_id');
+    }
 }
