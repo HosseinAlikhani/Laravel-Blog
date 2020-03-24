6cea7ce3e9ab12a107880d13b86bda3d7e0cd098
HosseinAlikhani <poshtiban.esteam@gmail.com>
2020-03-25 04:24:58 +0430

    chore - update: update migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ class CreateUsersTable extends Migration
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('email')->unique();
+            $table->text('pic')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('verification_code')->nullable();
             $table->string('password');
