8794d46ffc373a1c789bee9e36bf939d1597b7a1
HosseinAlikhani <poshtiban.esteam@gmail.com>
2020-03-25 03:12:07 +0430

    update: clean DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -3,9 +3,7 @@
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
-use App\User;
-use Spatie\Permission\Models\Permission;
-use Spatie\Permission\Models\Role;
+use App\Model\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,64 +16,12 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         DB::statement('SET FOREIGN_KEY_CHECKS=0;');
-        Role::truncate();
-        Permission::truncate();
         User::truncate();
         DB::statement('SET FOREIGN_KEY_CHECKS=1;');
-
-        for ($i = 1; $i < 5; $i++){
-            User::create([
-                'name'  =>  'd3cr33'.$i,
-                'email' =>  'd3cr33'.$i.'@gmail.com',
-                'password'  =>  Hash::make('123'),
-            ]);
-        }
-
-        $role = [
-            0   =>  [
-                'name'  =>  'admin',
-                'guard_name'    =>  'web',
-            ],
-            1   =>  [
-                'name'  =>  'user',
-                'guard_name'    =>  'web',
-            ],
-        ];
-        $permission = [
-            0   =>  [
-                'name'  =>  'create_role',
-                'guard_name'    =>  'web',
-            ],
-            1   =>  [
-                'name'  =>  'read_role',
-                'guard_name'    =>  'web',
-            ],
-            2   =>  [
-                'name'  =>  'update_role',
-                'guard_name'    =>  'web',
-            ],
-            3   =>  [
-                'name'  =>  'delete_role',
-                'guard_name'    =>  'web',
-            ],
-            4   =>  [
-                'name'  =>  'create_permission',
-                'guard_name'    =>  'web',
-            ],
-            5   =>  [
-                'name'  =>  'read_permission',
-                'guard_name'    =>  'web',
-            ],
-            6   =>  [
-                'name'  =>  'update_permission',
-                'guard_name'    =>  'web',
-            ],
-            7   =>  [
-                'name'  =>  'delete_permission',
-                'guard_name'    =>  'web',
-            ],
-        ];
-        Role::insert($role);
-        Permission::insert($permission);
+        User::create([
+            'name'  =>  'd3cr33',
+            'email' =>  'd3cr33@gmail.com',
+            'password'  =>  Hash::make('123456789'),
+        ]);
     }
 }
