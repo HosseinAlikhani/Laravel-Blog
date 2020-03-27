28d774fa204722010759705c1bcd949d81202754
HosseinAlikhani <poshtiban.esteam@gmail.com>
2020-03-27 17:49:32 +0430

    chore: clear section

--- routes/web.php
+++ routes/web.php
@@ -10,37 +10,8 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::prefix('profile')->group(function(){
-    Route::get('', 'Controller@profile');
-    Route::prefix('post')->group(function(){
-        Route::get('list', 'PostController@getPosts')->name('getPosts');
-        Route::get('edit/{post}', 'PostController@getPost')->name('editPost');
-        Route::get('add', 'PostController@getPostPost')->name('addPost');
-        Route::post('', 'PostController@postPost')->name('postPost');
-        Route::post('{post}', 'PostController@patchPost')->name('patchPost');
-        Route::delete('', 'PostController@deletePost')->name('deletePost');
-    });
-});
 
-Route::prefix('')->group(function(){
-    Route::get('blog', 'BlogController@blog')->name('blog');
-    Route::get('post/{id}', 'BlogController@single')->name('post');
-    Route::get('about', 'BlogController@about')->name('about');
-    Route::get('contact', 'BlogController@contact')->name('contact');
-    Route::get('work', 'BlogController@work')->name('work');
-});
 
-Route::prefix('comments')->group(function(){
-   Route::get('', 'CommentController@getComments');
-   Route::get('{comments}', 'CommentController@getComment');
-   Route::post('', 'CommentController@postComments')->name('postComment');
-   Route::patch('', 'CommentController@patchComments');
-   Route::delete('', 'CommentController@deleteComments');
-});
-Route::prefix('comment-replies')->group(function(){
-   Route::get('', 'CommentReplyController@getCommentReplies');
-   Route::get('{comment-replies}', 'CommentReplyController@getCommentReply');
-   Route::post('', 'CommentReplyController@postCommentReplies')->name('postCommentReply');
-   Route::patch('', 'CommentReplyController@patchCommentReplies');
-   Route::delete('', 'CommentReplyController@deleteCommentReplies');
-});
+
+
+
