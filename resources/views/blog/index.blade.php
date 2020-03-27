6bb1745924be1bc29bc7933e27c7bf5c91645620
HosseinAlikhani <poshtiban.esteam@gmail.com>
2020-03-27 23:05:43 +0430

    add: add bootstrap add fontawesomes

---
+++ resources/views/blog/index.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Meta Tag -->
+    <meta charset="UTF-8">
+    <!-- SEO -->
+    <meta name="description" content="personal website">
+    <meta name="author" content="d3cr33">
+    <meta name="url" content="http://www.d3cr33.com">
+    <meta name="copyright" content="Copyright This site is owned by d3cr33">
+    <meta name="robots" content="index,follow">
+    <title> D3CR33 - Personal Blog Template</title>
+
+    <!-- Main CSS Stylesheet -->
+    <link rel="stylesheet" type="text/css" href="{{ asset('devblog/css/style.css') }}">
+</head>
+
+
+<body>
+
+<script type="text/javascript" src="{{ asset('devblog/js/jquery.min.js') }}"></script>
+</body>
+</html>
